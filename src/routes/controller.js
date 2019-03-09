22e609a898342b5614cf708cc28b93abd4f376de
Adrian Zaragoza <adrianozaa@gmail.com>
2019-03-09 13:27:57 -0800

    add router

---
+++ src/routes/controller.js
@@ -0,0 +1,17 @@
+import Router from "helpers/router";
+
+// pages
+// import Index from "pages/Index.html";
+
+const router = new Router("index");
+
+// landing route
+router.route("/", context => router.view.set({ Page: Index }));
+
+// router.route("/app/:appname", context => router.view.set({
+//   Page: AppLoader,
+//   pageData: context.params.appname
+// }));
+
+
+export default router;
